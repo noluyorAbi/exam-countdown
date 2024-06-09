ccbd0a7f501bc454e5e625cad3feb40a63a48af1
Adatepe <adatepe.alperen.lmu@gmail.com>
2024-06-09 21:22:11 +0200

    fix type issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,34 @@
 import { useEffect, useState } from "react";
 import clsx from "clsx";
 
+interface Exam {
+  name: string;
+  date: string;
+}
+
+interface Countdown {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
 // Array with exam dates and times
-const examDates = [
+const examDates: Exam[] = [
   { name: "Bayes Statistik", date: "23.07.2024 10:00:00" },
   { name: "RNVS Exam", date: "26.07.2024 12:00:00" },
   { name: "BachelorSem", date: "28.06.2024 17:00:00" },
 ];
 
 // Convert dates to JavaScript Date objects
-const parseDate = (dateString) => {
+const parseDate = (dateString: string): Date => {
   const [day, month, yearAndTime] = dateString.split(".");
   const [year, time] = yearAndTime.split(" ");
   return new Date(`${year}-${month}-${day}T${time}`);
 };
 
 export default function Home() {
-  const [countdowns, setCountdowns] = useState(
+  const [countdowns, setCountdowns] = useState<Countdown[]>(
     examDates.map(() => ({ days: 0, hours: 0, minutes: 0, seconds: 0 }))
   );
 
@@ -28,7 +40,7 @@ export default function Home() {
           .map(({ date }) => parseDate(date))
           .map((date) => {
             const now = new Date();
-            const distance = date - now;
+            const distance = date.getTime() - now.getTime();
 
             const days = Math.floor(distance / (1000 * 60 * 60 * 24));
             const hours = Math.floor(
@@ -50,12 +62,12 @@ export default function Home() {
   // Sort exams by date
   const sortedExams = examDates
     .map((exam, index) => ({ ...exam, countdown: countdowns[index] }))
-    .sort((a, b) => parseDate(a.date) - parseDate(b.date));
+    .sort((a, b) => parseDate(a.date).getTime() - parseDate(b.date).getTime());
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 sm:p-8 md:p-16 lg:p-24 bg-gray-900 text-gray-100">
       {sortedExams.map((exam, index) => {
-        const timeLeft = parseDate(exam.date) - new Date();
+        const timeLeft = parseDate(exam.date).getTime() - new Date().getTime();
         const isLessThan4Weeks = timeLeft <= 4 * 7 * 24 * 60 * 60 * 1000;
         const isLessThan2Weeks = timeLeft <= 2 * 7 * 24 * 60 * 60 * 1000;
 
